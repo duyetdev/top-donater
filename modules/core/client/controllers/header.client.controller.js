044563d9b4757a789df80e951df7cc022b52729d
Van-Duyet Le <me@duyetdev.com>
2016-09-13 17:13:06 +0700

    Update header

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -5,15 +5,23 @@
     .module('core')
     .controller('HeaderController', HeaderController);
 
-  HeaderController.$inject = ['$scope', '$state', 'Authentication', 'menuService'];
+  HeaderController.$inject = ['$scope', '$state', '$location', 'Authentication', 'menuService'];
 
-  function HeaderController($scope, $state, Authentication, menuService) {
+  function HeaderController($scope, $state, $location,  Authentication, menuService) {
     var vm = this;
 
     vm.accountMenu = menuService.getMenu('account').items[0];
     vm.authentication = Authentication;
     vm.isCollapsed = false;
     vm.menu = menuService.getMenu('topbar');
+
+    vm.doLogin = function () {
+      $location.path('/authentication/signin');
+    }
+
+    vm.signOut = function () {
+      $location.path('/authentication/signin');
+    }
 
     $scope.$on('$stateChangeSuccess', stateChangeSuccess);
 
